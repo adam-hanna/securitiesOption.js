efdc4548da78b0c1a88682d26a42df8753ae1e96
Adam Hanna <adam@faze-1.com>
2014-10-17 23:34:29 -0400

    added: (1) interest object with interest specific methods; (2) black-scholes option pricing method; (3) a normDist function

--- README.md
+++ README.md
@@ -1,40 +1,68 @@
-securitiesOptions.js
-====================
+# securitiesOption.js
 
 
 securitiesOption.js is a javascript library for evaluating option prices.
 
 
-Objects:
---------
+## Classes:
 
+### 1. securitiesOption(oConstructor)
+
+This is an object that represents a securities option.
+
+#### Arguments
+oConstructor.Strike: //strike price of  the option
+oConstructor.CorP: //"call" or "put" ; case-insensitive
+
+#### Additional Properties - None. Same as arguments
+
+#### Example
 ```javascript
-securitiesOption
-	This is an object that represents an option.
-
-	Arguments
-    Strike: //strike price of  the option
-    CorP: //"call" or "put" ; case-insensitive
+var testOption = new securitiesOption({
+    Strike: 3000,
+    CorP: "call"
+});
 ```
 
+### 2. compoundInterest(oConstructor)
 
-Methods:
---------
+This is an object that represents an interest rate.
+
+#### Arguments
+oConstructor.Strike: //strike price of  the option
+oConstructor.CorP: //"call" or "put" ; case-insensitive
+
+#### Additional Properties - None. Same as arguments
+
+#### Example
+```javascript
+var testOption = new securitiesOption({
+    Strike: 3000,
+    CorP: "call"
+});
+```
+
+## Methods:
 
 ```javascript
 securitiesOption.trinomialCRR(Spot, Sigma, Rf, Days, Nodes)
-	This method calculates the trinomial Cox Ross Rubinstein value of the option.
+	This method calculates the trinomial Cox Ross Rubinstein value of the option. This method cannot account for dividends!
 
 	Arguments
     Spot: //spot price of the underlying
 	Sigma: //volatility
-    Rf: //risk free rate
+    Rf: //continuously compounded risk free rate
     Days: //days until expiry
     Nodes: //[OPTIONAL] the number of trinomial nodes to generate (Default = 800)
 ```
 
-Example:
---------
+## Functions:
+
+```javascript
+
+```
+
+## Example:
 	
 ```html
 <!--HTML-->
@@ -44,8 +72,17 @@ Example:
 	    CorP: "call"
 	});
 
+	var testInterest = new compoundInterest({
+		R: 0.06,
+		Freq: 12
+	});
+
 	console.log(testOption.trinomialCRR(1418.16, 0.3702, 0.00261, 854, 800));
-</script>~~
+	console.log(testOption.BS(1418.16, 0.3702, 0.00261, 0, 854));
+	console.log(testInterest.futureValue(100, 10));
+	console.log(testInterest.Rcont);
+	console.log(testInterest.Reff);
+</script>
 ```
 
-- This is a work in progress -
+#### Note: this is a work in progress
--- securitiesOption.js
+++ securitiesOption.js
@@ -1,68 +1,133 @@
+//Classes
 function securitiesOption(oConstructor) {
     this.Strike = oConstructor.Strike;
-    this.CorP = oConstructor.CorP.toLowerCase();   
+    this.CorP = oConstructor.CorP.toLowerCase();
+};
 
-	this.trinomialCRR = function(Spot, Sigma, Rf, Days, Nodes) {
-		Nodes = Math.round((Nodes || 800));
-		var Delta_t_yrs, Up, Down, Pu, Pd, Pm, Max_up, No_Underlying;
+function compoundInterest(oConstructor) {
+	this.Freq = oConstructor.Freq;
+	this.R = oConstructor.R;
+	this.Rcont = this.continuousRate();
+	this.Reff = this.effectiveRate();
+};
 
-		Delta_t_yrs = (Days / 365 / Nodes);
-		Up = (Math.exp((Sigma * Math.sqrt(2 * Delta_t_yrs))));
-		Down = (1 / Up);
-		Pu = (Math.pow((Math.exp(Rf * Delta_t_yrs / 2) - (Math.exp((-Sigma) * Math.sqrt(Delta_t_yrs / 2)))) / ((Math.exp(Sigma * Math.sqrt(Delta_t_yrs / 2))) - Math.exp((-Sigma) * Math.sqrt(Delta_t_yrs / 2))),2));
-		Pd = (Math.pow(((Math.exp(Sigma * Math.sqrt(Delta_t_yrs / 2)) - Math.exp(Rf * Delta_t_yrs / 2)) / (Math.exp(Sigma * Math.sqrt(Delta_t_yrs / 2)) - Math.exp((-Sigma) * Math.sqrt(Delta_t_yrs / 2)))),2));
-		Pm = (1 - Pu - Pd);
-		Max_up = (Spot * Math.pow(Up, Nodes));
-		No_Underlying = (Nodes * 2);
 
-		console.log(Delta_t_yrs, Up, Down, Pu, Pd, Pm, Max_up, No_Underlying);
+//Methods
+securitiesOption.prototype.trinomialCRR = function(Spot, Sigma, Rf, Days, Nodes) {
+	Nodes = Math.round((Nodes || 800));
+	var Delta_t_yrs, Up, Down, Pu, Pd, Pm, Max_up, No_Underlying;
 
-		var Underlying = [];
-		var Derivative = [];
+	Delta_t_yrs = (Days / 365 / Nodes);
+	Up = (Math.exp((Sigma * Math.sqrt(2 * Delta_t_yrs))));
+	Down = (1 / Up);
+	Pu = (Math.pow((Math.exp(Rf * Delta_t_yrs / 2) - (Math.exp((-Sigma) * Math.sqrt(Delta_t_yrs / 2)))) / ((Math.exp(Sigma * Math.sqrt(Delta_t_yrs / 2))) - Math.exp((-Sigma) * Math.sqrt(Delta_t_yrs / 2))),2));
+	Pd = (Math.pow(((Math.exp(Sigma * Math.sqrt(Delta_t_yrs / 2)) - Math.exp(Rf * Delta_t_yrs / 2)) / (Math.exp(Sigma * Math.sqrt(Delta_t_yrs / 2)) - Math.exp((-Sigma) * Math.sqrt(Delta_t_yrs / 2)))),2));
+	Pm = (1 - Pu - Pd);
+	Max_up = (Spot * Math.pow(Up, Nodes));
+	No_Underlying = (Nodes * 2);
 
-		//Input the final values of the underlying into the underlying array
-		for (var i = 0; i <= No_Underlying; i++) {
-			Underlying[i] = (Max_up * Math.pow(Down, i));
-		};
+	var Underlying = [];
+	var Derivative = [];
 
-		//populate the trinomial tree
-		if (this.CorP === "call") {
-		    //start with the leaves
-		    Derivative[Nodes] = [];
-
-		    for (var i = 0; i <= No_Underlying; i++) {
-		    	Derivative[Nodes][i] = Math.max((Underlying[i] - this.Strike), 0);
-		    };
-		    
-		    //now the branches
-		    for (var k = 1; Nodes - k >= 0; k++) {
-		    	Derivative[Nodes - k] = [];
-
-		        for (var i = k; i <= No_Underlying - k; i++) {
-		            Derivative[Nodes - k][i] = (Math.max((Underlying[i] - this.Strike), (Math.exp(((-Rf) * Delta_t_yrs))) * (Pu * Derivative[Nodes - k + 1][i - 1] + Pd * Derivative[Nodes - k + 1][i + 1] + Pm * Derivative[Nodes - k + 1][i])));
-		        };
-		    };
-		} else if (this.CorP === "put") {
-			 //start with the leaves
-		    Derivative[Nodes] = [];
-
-		    for (var i = 0; i <= No_Underlying; i++) {
-		    	Derivative[Nodes][i] = Math.max((this.Strike - Underlying[i]), 0);
-		    };
-		    
-		    //now the branches
-		    for (var k = 1; Nodes - k >= 0; k++) {
-		    	Derivative[Nodes - k] = [];
-
-		        for (var i = k; i <= No_Underlying - k; i++) {
-		            Derivative[Nodes - k][i] = (Math.max((this.Strike - Underlying[i]), (Math.exp(((-Rf) * Delta_t_yrs))) * (Pu * Derivative[Nodes - k + 1][i - 1] + Pd * Derivative[Nodes - k + 1][i + 1] + Pm * Derivative[Nodes - k + 1][i])));
-		        };
-		    };
-		} else {
-			return "Error!";
-		};
-
-		return Derivative[0][Nodes];
-
+	//Input the final values of the underlying into the underlying array
+	for (var i = 0; i <= No_Underlying; i++) {
+		Underlying[i] = (Max_up * Math.pow(Down, i));
 	};
+
+	//populate the trinomial tree
+	if (this.CorP === "call") {
+	    //start with the leaves
+	    Derivative[Nodes] = [];
+
+	    for (var i = 0; i <= No_Underlying; i++) {
+	    	Derivative[Nodes][i] = Math.max((Underlying[i] - this.Strike), 0);
+	    };
+	    
+	    //now the branches
+	    for (var k = 1; Nodes - k >= 0; k++) {
+	    	Derivative[Nodes - k] = [];
+
+	        for (var i = k; i <= No_Underlying - k; i++) {
+	            Derivative[Nodes - k][i] = (Math.max((Underlying[i] - this.Strike), (Math.exp(((-Rf) * Delta_t_yrs))) * (Pu * Derivative[Nodes - k + 1][i - 1] + Pd * Derivative[Nodes - k + 1][i + 1] + Pm * Derivative[Nodes - k + 1][i])));
+	        };
+	    };
+	} else if (this.CorP === "put") {
+		 //start with the leaves
+	    Derivative[Nodes] = [];
+
+	    for (var i = 0; i <= No_Underlying; i++) {
+	    	Derivative[Nodes][i] = Math.max((this.Strike - Underlying[i]), 0);
+	    };
+	    
+	    //now the branches
+	    for (var k = 1; Nodes - k >= 0; k++) {
+	    	Derivative[Nodes - k] = [];
+
+	        for (var i = k; i <= No_Underlying - k; i++) {
+	            Derivative[Nodes - k][i] = (Math.max((this.Strike - Underlying[i]), (Math.exp(((-Rf) * Delta_t_yrs))) * (Pu * Derivative[Nodes - k + 1][i - 1] + Pd * Derivative[Nodes - k + 1][i + 1] + Pm * Derivative[Nodes - k + 1][i])));
+	        };
+	    };
+	} else {
+		return "Error!";
+	};
+
+	return Derivative[0][Nodes];
+};
+
+securitiesOption.prototype.BS = function(Spot, Sigma, Rf, Rd, Days) {
+	//note: the natural log is implemented with Math.log in javascript
+	var Nd1 = normDist((Math.log(Spot/this.Strike)+Days/365*(Rf-Rd+Math.pow(Sigma,2)/2))/(Sigma*Math.pow((Days/365),0.5)));
+	var Nd2 = normDist(((Math.log(Spot/this.Strike)+Days/365*(Rf-Rd+Math.pow(Sigma,2)/2))/(Sigma*Math.pow((Days/365),0.5)))-Sigma*Math.pow((Days/365),0.5));
+	var PVx	= this.Strike*Math.exp(-Rf*Days/365);
+
+	var BS = null;
+
+	if (this.CorP === "put") {
+		BS = Spot*Math.exp(-Rd*Days/365)*(Nd1-1)-this.Strike*(Nd2-1)*Math.exp(-Rf*Days/365);
+	} else {
+		BS = Spot*Math.exp(-Rd*Days/365)*Nd1-this.Strike*Nd2*Math.exp(-Rf*Days/365);
+	};
+
+	return BS;
+};
+
+compoundInterest.prototype.continuousRate = function() {
+	return this.Freq * Math.log(1 + this.R / this.Freq);
+};
+
+compoundInterest.prototype.effectiveRate = function() {
+	return (Math.pow(1 + this.R / this.Freq, this.Freq) - 1);
+};
+
+compoundInterest.prototype.futureValue = function(P, t) {
+	return (P * Math.pow(1 + this.R / this.Freq, this.Freq * t));
+};
+
+
+//Functions
+function erf(x) {
+	//taken from: http://www.codeproject.com/Articles/408214/Excel-Function-NORMSDIST-z
+    //A&S formula 7.1.26
+    var a1 = 0.254829592;
+    var a2 = -0.284496736;
+    var a3 = 1.421413741;
+    var a4 = -1.453152027;
+    var a5 = 1.061405429;
+    var p = 0.3275911;
+    x = Math.abs(x);
+    var t = 1 / (1 + p * x);
+    //Direct calculation using formula 7.1.26 is absolutely correct
+    //But calculation of nth order polynomial takes O(n^2) operations
+    //return 1 - (a1 * t + a2 * t * t + a3 * t * t * t + a4 * t * t * t * t + a5 * t * t * t * t * t) * Math.Exp(-1 * x * x);
+
+    //Horner's method, takes O(n) operations for nth order polynomial
+    return (1 - ((((((a5 * t + a4) * t) + a3) * t + a2) * t) + a1) * t * Math.exp(-1 * x * x));
+
+};
+
+function normDist(z) {
+	//taken from: http://www.codeproject.com/Articles/408214/Excel-Function-NORMSDIST-z
+	var sign = 1;
+    if (z < 0) sign = -1;
+    return (0.5 * (1.0 + sign * erf(Math.abs(z)/Math.sqrt(2))));
 };
